834bbd1c2733080816c29494a0c01a892f80f18a
AS064678 <AS064678@cerner.net>
2021-02-13 23:04:29 +0530

    updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-(function () {
+(
+  function () {
   angular.module('application', [])
   .controller('lunchCheckController', controller);
   controller.$inject = ['$scope'];
@@ -9,8 +10,8 @@
     $scope.redColorOpacity = '';
     $scope.check = function() {
       let count = 0;
-      let a[i] = $scope.items.split(',');
-      a[i].forEach((item) => {
+      let arrayOfItems = $scope.items.split(',');
+      arrayOfItems.forEach((item) => {
       if (item!= false) {
         count++;
       }
