cbd0a4741104aa21a3254f62a0b8e5f6e5f9e513
AS064678 <AS064678@cerner.net>
2021-02-13 22:50:43 +0530

    Single Web page Development

---
+++ app.js
@@ -0,0 +1,37 @@
+(function () {
+  angular.module('application', [])
+  .controller('lunchCheckController', controller);
+  controller.$inject = ['$scope'];
+  function controller($scope) {
+    $scope.greenColorOpacity = '';
+    $scope.items = '';
+    $scope.messageNotification = '';
+    $scope.redColorOpacity = '';
+    $scope.check = function() {
+      let count = 0;
+      let a[i] = $scope.items.split(',');
+      a[i].forEach((item) => {
+      if (item!= false) {
+        count++;
+      }
+      });
+
+      if(count > 0 && count < 4) {
+        $scope.messageNotification = 'Enjoy';
+        $scope.redColorOpacity = '';
+        $scope.greenColorOpacity = 'opaque';
+      }
+      else if (count > = 4) {
+        $scope.messageNotification = 'Too much';
+        $scope.redColorOpacity = opaque';
+        $scope.greenColorOpacity = '';
+      }
+      else {
+        $scope.messageNotification = 'Please enter an item first';
+        $scope.redColorOpacity = opaque';
+        $scope.greenColorOpacity = '';
+      }
+    }
+  }
+}
+)()
